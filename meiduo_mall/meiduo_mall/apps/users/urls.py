be0ed7694a6f159cbaeae294f3a598b814dc6edd
15779500251 <1758264965@qq.com>
2019-08-24 17:39:39 +0800

    用户名重复注册

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -20,4 +20,7 @@ from users import views
 
 urlpatterns = [
 url(r'^register/$', views.RegisterView.as_view(), name='register'),
+url(r'^usernames/(?P<username>[a-zA-Z0-9_-]{5,20})/count/$'r'', views.UsernameCountView.as_view(), name='count'),
+url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view(), name='MobileCount'),
+
 ]
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,7 +1,11 @@
+from django import http
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
+
+from meiduo_mall.utils.response_code import RETCODE
+from users.models import User
 
 
 class RegisterView(View):
@@ -14,3 +18,31 @@ class RegisterView(View):
         :return: 注册界面
         """
         return render(request, 'register.html')
+
+class UsernameCountView(View):
+    # 判断用户名是否重复注册
+    def get(self,request,username):
+        '''
+
+        :param request: 请求对象
+        :param username: 用户名
+        :return: JSON
+        '''
+        # 获取数据库中用户名的个数
+        count = User.objects.filter(username=username).count()
+        return http.JsonResponse({'code':RETCODE.OK,'errmsg':'ok','count':count })
+
+class MobileCountView(View):
+    # 判断手机号是否重复注册
+    def get(self,ruquest,mobile):
+        '''
+
+        :param ruquest: 请求对象
+        :param mobile: 手机号
+        :return: JSON
+        '''
+        count = User.objects.filter(mobile=mobile).count()
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
+
+
+
